e9dcd9ba4541385c167569a264847c16f56b2474
unknown <jessica-kaelbly@web.de>
2019-12-12 16:00:54 +0100

    button komponente hinzugefügt

---
+++ client/app/component/HomeButton.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="buttons">
+
+        <button>Registrieren</button> 
+        <button>Anmelden</button>
+
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'buttons',
+    data () {
+        return {
+   
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>

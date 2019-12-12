6871daebb7f94bbe7dc588d6ea6a82d7d86b9780
adriannahlik <adrian.nahlik@gmx.de>
2019-12-12 16:12:04 +0100

    navbar eingefügt

--- client/app/App.vue
+++ client/app/App.vue
@@ -1,10 +1,21 @@
 <template>
   <div id="app">
       <h1>Scissoroo</h1>
-  <div id="test">
-    <img src="https://raw.githubusercontent.com/xnngyn/Scissoroo/master/public/images/Scissoroo_Logo.jpg" width="100" height="100" alt="">
-</div> 
+  
+		
+
   <body> 
+    <div id:="Test">
+
+    <nav class="navbar navbar-light bg-light">
+		  <a class="navbar-brand" href="/users/loggedIn"><img src="https://raw.githubusercontent.com/xnngyn/Scissoroo/master/public/images/Scissoroo_Logo.jpg" width="100" height="100" alt=""></a> <!--../Bilder/Scissoroo_Logo.jpg-->
+			<form class="form-inline">
+				<a class="btn btn-secondary mr-sm-2" href="/users/profile" role="button">Mein Profil</a>
+				<a class="btn btn-secondary"  href="/users/logout" role="button">Ausloggen</a>
+			</form>
+      </nav>	
+		  </div>
+      
     <hr>
     <router-link to="/">Home</router-link>
     <router-link to="/signin">Einloggen</router-link>
